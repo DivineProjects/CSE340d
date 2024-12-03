aa1fcb60778f26ceff6530ed462727aaac6b2eff
divineProjects <divinejigu@gmail.com>
2024-12-03 23:06:22 +0200

    error handling added

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 const static = require("./routes/static") // contains routes directory to css, js and images 
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities")
 
 /* ***********************
  * View Egine and Templates
@@ -26,15 +27,29 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 // index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 // inventory routes
 app.use("/inv", inventoryRoute)
-// app.get("/", function(req,res) {
-//   res.render("index", {title: "Home"})
-// })
-// const baseController = require("../controllers/baseController");
 
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 
 /* ***********************
