b04fe6f80223648b8ae278fef7233a402809345e
divineProjects <divinejigu@gmail.com>
2024-12-04 05:12:30 +0200

    update

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -2,7 +2,7 @@ const utilities = require("../utilities/")
 const errorController = {}
 
 errorController.triggerError = async function (req, res) {
-    // const nav = await utilities.getNav()
+    const nav = await utilities.getNav()
     res.render("index", {title: "Home", nav})
 }
 
