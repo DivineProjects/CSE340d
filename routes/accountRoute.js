2359fed3876b48164f719b2ba7a66d78f8321707
divineProjects <divinejigu@gmail.com>
2024-12-13 03:09:11 +0200

    validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,10 +3,25 @@ const express = require("express")
 const router = new express.Router() 
 const accController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validation')
 
 // Route to build Account Login View
-router.get("/login", accController.buildLogin);
+router.get("/login", accController.buildLogin)
 router.get("/register", accController.buildRegister)
-router.post("/register", utilities.handleErrors(accController.registerAccount))
+router.post("/register",
+    regValidate.registationRules(), 
+    regValidate.checkRegData, 
+    utilities.handleErrors(accController.registerAccount)
+)
 
+
+// Process the login attempt
+router.post(
+    "/login", 
+    regValidate.loginRules(),
+    regValidate.checkLoginData,
+    (req, res) => {
+      res.status(200).send('login process')
+    }
+  )
 module.exports = router;
